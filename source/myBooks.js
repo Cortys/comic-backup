d9887ecadc9e48c00cd51fff3ec34f63323323cf
Marvin <github@gfield.de>
2014-05-08 22:55:58 +0200

    Fixed the new download button

--- source/myBooks.js
+++ source/myBooks.js
@@ -5,8 +5,8 @@ for (i in readButtons)
 	(function(i){
 		var clone = readButtons[i].cloneNode(false);
 		clone.innerHTML = "Download";
-		clone.style.width = (readButtons[i].clientWidth - 20) + "px";
-		clone.style.textAlign = "center";
+		clone.style.width = readButtons[i].style.width = (readButtons[i].clientWidth - 20) + "px";
+		clone.style.textAlign = readButtons[i].style.textAlign = "center";
 		clone.href = "javascript:";
 		clone.addEventListener("click", function(){
 			chrome.runtime.sendMessage({"what": "open_background_tab", "url": readButtons[i].href}, function(tab){
@@ -15,5 +15,5 @@ for (i in readButtons)
 				});
 			});
 		}, false);
-		readButtons[i].parentElement.appendChild(clone);	
+		readButtons[i].parentNode.insertBefore(clone, readButtons[i].nextSibling);	
 	})(i)
