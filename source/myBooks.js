833589866333695c9f9e5232f3bcf76ca2694494
Marvin <github@gfield.de>
2014-05-08 20:44:25 +0200

    .

--- source/myBooks.js
+++ source/myBooks.js
@@ -1,8 +1,19 @@
-var readButtons = document.body.querySelectorAll(".read-comic.titleBtn");
-
-
-var i;
+var readButtons = document.body.querySelectorAll(".read-comic.titleBtn"),
+	i;
 for (i in readButtons){
-	var clone = readButtons[i].cloneNode();
-	readButtons[i].insertAfter(clone);
+	
+}
+
+for (i in readButtons){ 
+	(function(i){
+		var clone = readButtons[i].cloneNode(false);
+		clone.innerHTML = "Download";
+		clone.style.width = (readButtons[i].clientWidth - 20) + "px";
+		clone.style.textAlign = "center";
+		clone.href = "javascript:";
+		clone.addEventListener("click", function(){
+			chrome.tabs.create({'url': readButtons[i].href, 'active': false}, function(tab) {});
+		}, false);
+		readButtons[i].parentElement.appendChild(clone);	
+	})(i)
 }
