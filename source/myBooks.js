75ffc97484318ec1f72ec511c00496999cadf04f
Marvin <github@gfield.de>
2014-05-11 03:49:46 +0200

    Fast buttons all over the place

--- source/myBooks.js
+++ source/myBooks.js
@@ -1,36 +1,45 @@
-var readButtons = document.body.querySelectorAll(".read-comic.titleBtn"),
-	i,
-	tabDownloaders = {},
-	addButton = function(button){
-		var clone = button.cloneNode(false);
-		clone.innerHTML = "Download";
-		clone.style.width = button.style.width = window.getComputedStyle(button).width;
-		clone.style.textAlign = button.style.textAlign = "center";
-		clone.href = "javascript:";
-		clone.addEventListener("click", function(){
-			chrome.runtime.sendMessage({"what": "open_background_tab", "url": button.href}, function(tab) {
-				tabDownloaders[tab.id] = function(sendResponse) {
-					sendResponse({download: true});
-				};
-			});
-		}, false);
-		button.parentNode.insertBefore(clone, button.nextSibling);
-		return clone;
-	};
+var exceptions = {
+	"": true,
+	"/": true,
+	"/comic-reader/": true
+};
 
 
-for(i = 0; i < readButtons.length; i++)
-	(function(e){
-		addButton(e);
-	})(readButtons[i]);
+if( !exceptions[location.pathname.split("/"[1], 1)] ){
+	var readButtons = document.body.querySelectorAll(".read-comic.titleBtn, .read_link"),
+		i,
+		tabDownloaders = {},
+		addButton = function(button){
+			var clone = button.cloneNode(false);
+			clone.innerHTML = "Download";
+			clone.style.width = button.style.width = window.getComputedStyle(button).width;
+			clone.style.textAlign = button.style.textAlign = "center";
+			clone.href = "javascript:";
+			clone.addEventListener("click", function(){
+				chrome.runtime.sendMessage({"what": "open_background_tab", "url": button.href}, function(tab) {
+					tabDownloaders[tab.id] = function(sendResponse) {
+						sendResponse({download: true});
+					};
+				});
+			}, false);
+			button.parentNode.insertBefore(clone, button.nextSibling);
+			return clone;
+		};
+	
+	
+	for(i = 0; i < readButtons.length; i++)
+		(function(e){
+			addButton(e);
+		})(readButtons[i]);
 
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-	if(request.what == "ready_to_download" && tabDownloaders[request.tab.id]) {
-		tabDownloaders[request.tab.id](sendResponse);
-		delete tabDownloaders[request.tab.id];
-		return true;
-	}
-	else if(request.what == "finished_download")
-		chrome.runtime.sendMessage({ what:"close_background_tab", tab:request.tab });
-});
+	chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+		if(request.what == "ready_to_download" && tabDownloaders[request.tab.id]) {
+			tabDownloaders[request.tab.id](sendResponse);
+			delete tabDownloaders[request.tab.id];
+			return true;
+		}
+		else if(request.what == "finished_download")
+			chrome.runtime.sendMessage({ what:"close_background_tab", tab:request.tab });
+	});
+}
