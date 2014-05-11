9e8181d35f393e71bf08d284dcd4edaceb4c9a43
Marvin <github@gfield.de>
2014-05-11 03:25:07 +0200

    Made the script more flexible

--- source/myBooks.js
+++ source/myBooks.js
@@ -1,23 +1,28 @@
 var readButtons = document.body.querySelectorAll(".read-comic.titleBtn"),
 	i,
-	tabDownloaders = {};
-
-for(i = 0; i < readButtons.length; i++)
-	(function(e){
-		var clone = e.cloneNode(false);
+	tabDownloaders = {},
+	addButton = function(button, width){ //TODO incorporate correct button widths
+		var clone = button.cloneNode(false);
 		clone.innerHTML = "Download";
-		clone.style.width = e.style.width = (e.clientWidth - 20) + "px";
-		clone.style.textAlign = e.style.textAlign = "center";
+		clone.style.width = button.style.width = parseInt(window.getComputedStyle(button).width);
+		clone.style.textAlign = button.style.textAlign = "center";
 		clone.href = "javascript:";
 		clone.addEventListener("click", function(){
-			chrome.runtime.sendMessage({"what": "open_background_tab", "url": e.href}, function(tab) {
+			chrome.runtime.sendMessage({"what": "open_background_tab", "url": button.href}, function(tab) {
 				tabDownloaders[tab.id] = function(sendResponse) {
-					sendResponse({ download:true });
+					sendResponse({download: true});
 				};
 			});
 		}, false);
-		e.parentNode.insertBefore(clone, e.nextSibling);
+		button.parentNode.insertBefore(clone, button.nextSibling);
+	};
+
+
+for(i = 0; i < readButtons.length; i++)
+	(function(e){
+		addButton(e);
 	})(readButtons[i]);
+
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 	if(request.what == "ready_to_download" && tabDownloaders[request.tab.id]) {
