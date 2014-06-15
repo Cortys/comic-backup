7d2386a37d344c93005fadefeaac64d9ba9cda81
Clemens Damke <admin@cortys.de>
2014-06-15 21:39:14 +0200

    Code rearranged
    + Little design issue related to the gift button fixed

--- source/myBooks.js
+++ source/myBooks.js
@@ -4,7 +4,7 @@ var exceptions = {
 };
 
 
-if(!exceptions[location.pathname.split("/", 1)[1]]){
+if(!exceptions[location.pathname.split("/", 1)[1]]) {
 	var cssClass = randomString(20, 40),
 		injectCss = function(){
 			var style = document.createElement("style");
@@ -13,20 +13,20 @@ if(!exceptions[location.pathname.split("/", 1)[1]]){
 			document.documentElement.insertBefore(style);
 		},
 		readButtons = document.body.querySelectorAll(".read-comic.titleBtn, .read_link"),
-		i,
 		downloadEvents = {},
-		Download = function(button){
+		Download = function(button) {
 			this.readButton = button;
 			var clone = this.downloadButton = button.cloneNode(false),
 				t = this,
 				hasGiftButton = false,
-				giftButton = button.nextSibling;
+				giftButton = button.nextSibling,
+				giftComputedStyle;
 			clone.innerHTML = "Download";
-			clone.style.width = (parseInt(button.style.width = window.getComputedStyle(button).width) + (giftButton && typeof giftButton.className == "string" && (hasGiftButton =  giftButton.className.match(/gift_link/g)) && parseInt(window.getComputedStyle(giftButton).width) || 0)) + "px";
+			clone.style.width = (parseInt(button.style.width = window.getComputedStyle(button).width) + (giftButton && typeof giftButton.className == "string" && (hasGiftButton = giftButton.className.match(/gift_link/g)) && (giftComputedStyle = window.getComputedStyle(giftButton)) && parseInt(giftComputedStyle.width)+parseInt(giftComputedStyle.marginLeft)+parseInt(giftComputedStyle.marginRight) || 0)) + "px";
 			clone.style.textAlign = button.style.textAlign = "center";
-			clone.style.marginTop = "1px";
+			clone.style.marginTop = giftComputedStyle?giftComputedStyle.marginLeft:"1px";
 			clone.href = "javascript:";
-			clone.addEventListener("click", function(){
+			clone.addEventListener("click", function() {
 				t.events["start_download"].call(t);
 			}, false);
 			clone.classList.add(cssClass);
@@ -40,32 +40,34 @@ if(!exceptions[location.pathname.split("/", 1)[1]]){
 		downloadButton: null,
 		tab: null,
 		events: {
-			"ready_to_download": function(sendResponse){
+			"ready_to_download": function(sendResponse) {
 				sendResponse({download: true});
-			}, "finished_download": function(){
+			}, "finished_download": function() {
 				chrome.runtime.sendMessage({ what: "close_background_tab", tab: this.tab });
-			}, "closed_background_tab": function(){
+			}, "closed_background_tab": function() {
 				delete downloadEvents[this.tab.id];
 				this.tab = null;
-			}, "start_download": function(){
+			}, "start_download": function() {
 				var t = this;
 				chrome.runtime.sendMessage({what: "open_background_tab", url: this.readButton.href}, function(tab) {
 					t.tab = tab;
 					downloadEvents[tab.id] = t;
 				});
-			}, "download_progress": function(sendResponse, percentage){
+			}, "download_progress": function(sendResponse, percentage) {
 				this.downloadButton.innerHTML = percentage + "%";
 			}
 		}
 	};
-
-	for(i = 0; i < readButtons.length; i++)
-		(function(e){
-			new Download(e);
-		})(readButtons[i]);
-
-
+	
+	for(var i = 0; i < readButtons.length; i++)
+		new Download(readButtons[i]);
+	
 	chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-		return downloadEvents[request.tab.id] && typeof downloadEvents[request.tab.id].events[request.what] == "function" && downloadEvents[request.tab.id].events[request.what].call(downloadEvents[request.tab.id], sendResponse, request.data);
+		return downloadEvents[request.tab.id] && typeof downloadEvents[request.tab.id].events[request.what] == "function" &&
+			downloadEvents[request.tab.id].events[request.what].call(
+				downloadEvents[request.tab.id],
+				sendResponse,
+				request.data
+			);
 	});
 }
