8d1ebeaf66f888b66505f2ced83cf4b586bcf54d
Clemens <admin@cortys.de>
2014-12-17 00:00:14 +0100

    Setup button shown for every comic now.
    Thanks to @mattjmorrison.

--- source/myBooks.js
+++ source/myBooks.js
@@ -85,16 +85,12 @@ getSettings(function() {
 				}, false);
 				t.showDefault();
 			}
-			else if(!this.id) {
+			else {
 				t.inactive = true;
 				clone.addEventListener("click", function() {
 					t.openTab(true);
 				}, false);
 				t.showInactive();
-			}
-			else {
-				clone.href = "#";
-				t.showUnusable();
 			}
 		};
 
@@ -255,15 +251,6 @@ getSettings(function() {
 			this.downloadButton.style.removeProperty("background");
 			this.downloadButton.style.filter = this.downloadButton.style.webkitFilter = "hue-rotate(195deg)";
 			this.text.innerHTML = "Setup Scanner";
-			this.setCancelable(false);
-		},
-		showUnusable: function() {
-			this.downloadButton.style.background = this.buttonBGs.gray;
-			this.downloadButton.style.border = "1px solid #555555";
-			this.downloadButton.style.cursor = "default";
-			this.downloadButton.style.removeProperty("filter");
-			this.downloadButton.style.removeProperty("-webkit-filter");
-			this.text.innerHTML = "Setup Required";
 			this.setCancelable(false);
 		}
 	};
