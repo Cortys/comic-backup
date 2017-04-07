afb219e067cfa20c37aa6396953f56a7bb80f588
Clemens Damke <admin@cortys.de>
2017-04-07 18:35:01 +0200

    removed beforeunload dialog from reader
    this also affects normal reader tabs because implementing a differentiation between both types of tabs would give cmxlgy an indicator, that the extension is being used, which would enable them to identify users.

--- source/reactivateDom.js
+++ source/reactivateDom.js
@@ -3,6 +3,30 @@
 var script = document.createElement("script");
 script.type = "text/javascript";
 script.id = randomString(20, 40);
-script.innerHTML = "(function(){var f=function MutationObserver(){},sp=Event.prototype.stopPropagation;f.prototype.observe=function(){};window.MutationObserver=f;Object.defineProperty(window, 'MutationObserver', {value:f,writable:false,configurable:false});Event.prototype.stopPropagation=function stopPropagation(){if(!document.documentElement.hasAttribute('scanning'))sp.apply(this,arguments);};document.documentElement.removeChild(document.getElementById('" + script.id + "'));})()";
+script.innerHTML = `(function() {
+	var f = function MutationObserver() {},
+		sp = Event.prototype.stopPropagation,
+		ael = EventTarget.prototype.addEventListener;
+
+	f.prototype.observe = function() {};
+
+	window.MutationObserver = f;
+	Object.defineProperty(window, 'MutationObserver', {
+		value: f,
+		writable: false,
+		configurable: false
+	});
+
+	Event.prototype.stopPropagation = function stopPropagation() {
+		if(!document.documentElement.hasAttribute('scanning'))
+			sp.apply(this, arguments);
+	};
+	EventTarget.prototype.addEventListener = function addEventListener(type) {
+		if(type !== "beforeunload")
+			return ael.apply(this, arguments);
+	};
+
+	document.documentElement.removeChild(document.getElementById("${script.id}"));
+}())`;
 
 document.documentElement.insertBefore(script, document.documentElement.firstChild);
