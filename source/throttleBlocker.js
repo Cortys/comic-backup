a8f962ce231b103084d4eacfb48edbcdb85e4d3a
Clemens Damke <admin@cortys.de>
2018-08-30 19:29:39 +0200

    now uses WebRTC instead of WebAudio to prevent background throttling
    Starting with Chrome 70 WebAudio requires user interaction to work. The WebRTC channels that are now used instead, currently do nothing because simply opening a channel seems to be sufficient (at least for the moment).

--- source/throttleBlocker.js
+++ source/throttleBlocker.js
@@ -6,16 +6,5 @@ function throttleBlocker() {
 
 	throttleBlocker.active = true;
 
-	var ctx = new AudioContext();
-	var o = ctx.createOscillator();
-	var g = ctx.createGain();
-
-	o.type = "sine";
-	o.frequency.value = 1;
-	g.gain.value = 0.001;
-
-	o.connect(g);
-	g.connect(ctx.destination);
-
-	o.start();
+	new RTCPeerConnection();
 }
