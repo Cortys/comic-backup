ea7a50c6f6521a750f5ce8c15eaf9484ebc332d3
Clemens Damke <cdamke@mail.upb.de>
2021-08-04 16:26:09 +0200

    improved throttle blocker see https://developer.chrome.com/blog/timer-throttling-in-chrome-88/

--- source/throttleBlocker.js
+++ source/throttleBlocker.js
@@ -1,10 +1,20 @@
 "use strict";
 
 function throttleBlocker() {
-	if(typeof AudioContext !== "function" || throttleBlocker.active)
+	if(typeof RTCPeerConnection !== "function" || throttleBlocker.active)
 		return;
 
 	throttleBlocker.active = true;
 
-	new RTCPeerConnection();
+	var connA = new RTCPeerConnection();
+	var connB = new RTCPeerConnection();
+	connA.createDataChannel("dummy_channel");
+	connA.onicecandidate = e => !e.candidate || connB.addIceCandidate(e.candidate);
+    connB.onicecandidate = e => !e.candidate || connA.addIceCandidate(e.candidate);
+	connA.createOffer()
+		.then(offer => connA.setLocalDescription(offer))
+		.then(() => connB.setRemoteDescription(connA.localDescription))
+		.then(() => connB.createAnswer())
+		.then(answer => connB.setLocalDescription(answer))
+		.then(() => connA.setRemoteDescription(connB.localDescription));
 }
